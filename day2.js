36f3b11f091f092ce87683128a170ab65c2cf7a0
Caitlin Baldini <cat@listoglobal.com>
2024-01-10 17:00:51 -0700

    loops test

--- day2.js
+++ day2.js
@@ -1,3 +1,18 @@
-const meals = ["artichokes", "bbq", "pizza", "donuts"];
+const meals = ["1", "2", "3", "4", "5"];
 
-console.log(meals[0]);
+for (let i = 6; i >= 0; i -= 1) {
+  console.log(meals[i]);
+}
+console.log("BLAST OFF");
+
+const grid = [
+  ["A", "B", "C"],
+  ["D", "E", "F"],
+  ["G", "H", "I"],
+];
+
+for (let i = 0; i < grid.length; i++) {
+  for (let j = 0; j < grid[i].length; j++) {
+    console.log(grid[i][j]);
+  }
+}
